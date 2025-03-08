2f7d822db45edf06ed0c29d82006574b2845cfce
feerose111 <feerose009@gmail.com>
2025-03-08 20:28:26 +0545

    front-end 1

--- handynepal/app/urls.py
+++ handynepal/app/urls.py
@@ -3,8 +3,10 @@ from .views import  *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('home1/', home1, name='home1'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-
+    path('login/', user_login, name='user_login'),
+    path('registration/', user_registration, name='user_registration'),
+    path('collections/', collections, name='collections'),
+    path('artisans/', artisans, name='artisans'),
 ]
--- handynepal/app/views.py
+++ handynepal/app/views.py
@@ -5,13 +5,22 @@ from django.shortcuts import render , redirect
 def home(request):
     return render(request, 'main/home.html')
 
-def home1(request):
-    return render(request, 'home1.html')
+def collections(request):
+    return render(request, 'main/collections.html')
+
+def artisans(request):
+    return render(request, 'main/artisans.html')
 
 def about(request):
     return render(request, 'main/about.html')
 
 def contact(request):
     return render(request, 'main/contact.html')
+
+def user_login(request):
+    return render(request, 'authentication/login.html')
+
+def user_registration(request):
+    return render(request, 'authentication/registration.html')
 
 
