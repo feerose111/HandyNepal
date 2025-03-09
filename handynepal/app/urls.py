57000fecb3f0b6e258dac9e05b681c9b47b78f9d
feerose111 <feerose009@gmail.com>
2025-03-09 16:26:15 +0545

    day 3

--- handynepal/app/urls.py
+++ handynepal/app/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('registration/', user_registration, name='user_registration'),
     path('collections/', collections, name='collections'),
     path('artisans/', artisans, name='artisans'),
+    path('dashboard/', user_dashboard, name='user_dashboard'),
+    path('faq/', faq, name='faq'),
+    path('shipping_returns/', shipping_returns, name='shipping_returns'),
+    path('payment/', payment, name='payment'),
+    path('terms_and_conditions/', terms_and_conditions, name='terms_and_conditions'),
+    path('privacy_policy/', privacy_policy, name='privacy_policy'),
 ]
--- handynepal/app/views.py
+++ handynepal/app/views.py
@@ -17,10 +17,33 @@ def about(request):
 def contact(request):
     return render(request, 'main/contact.html')
 
+def user_dashboard(request):
+    return render(request, 'main/dashboard.html')
+
 def user_login(request):
     return render(request, 'authentication/login.html')
 
 def user_registration(request):
     return render(request, 'authentication/registration.html')
+
+
+#help section
+
+def faq(request):
+    return render(request, 'help/faq.html')
+
+def shipping_returns(request):
+    return render(request, 'help/shipping_returns.html')
+
+def payment(request):
+    return render(request, 'help/payment.html')
+
+def terms_and_conditions(request):
+    return render(request, 'help/terms_and_conditions.html')
+
+def privacy_policy(request):
+    return render(request, 'help/privacy_policy.html')
+
+
 
 
