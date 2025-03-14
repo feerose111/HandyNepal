cf3f7b3817ff7cbbc088a514f683849763661dcf
feerose111 <feerose009@gmail.com>
2025-03-14 09:06:20 +0545

    fresh commit from start

--- handynepal/settings.py
+++ handynepal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#ikf8s^*_e%$tf#^lx8z3e(kiay#!8_ld^bcihfhj@huqc4%g6'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-#ikf8s^*_e%$tf#^lx8z3e(kiay#!8_ld^bcihfhj@huqc4%g6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 
 # Application definition
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'handynepal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'handynepal',
-        'USER': 'postgres',
-        'PASSWORD': 'admin@123',
-        'HOST': 'localhost', 
-        'PORT': '5432', 
+        'NAME': config('DB_NAME', default='handynepal'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='admin@123'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
